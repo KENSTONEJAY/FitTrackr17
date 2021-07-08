48622b6a318e8bcf8b6fb2dd0f62e3301381d300
Ken Otuo <mrkoajr@gmail.com>
2021-07-08 18:26:41 -0400

    formatting and fixing typos

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://local/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
